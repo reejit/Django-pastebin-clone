faaf37ea4ed3a671c015bc5c81507f7c42031dbb
MohamedAyman3600 <moh.ayman3600@gmail.com>
2019-07-23 21:20:24 +0200

    Add class SnippetList APIView

--- pastbinAPI/pastebin/views.py
+++ pastbinAPI/pastebin/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from .models import Snippet
+from .serializers import SnipprtSerializer
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class SnippetList(APIView):
+    def get(self, request, format=None):
+        snippets = Snippet.objects.all()
+        serilaizer = SnipprtSerializer(snippets, many=True)
+        return Response(serilaizer.data)
+
+    def post(self, request, format=None):
+        serializer = SnipprtSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.sav()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
