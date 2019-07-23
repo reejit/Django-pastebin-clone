f162a2f11b3aa9c26fbde7dfa4669212037e8c4c
MohamedAyman3600 <moh.ayman3600@gmail.com>
2019-07-23 16:48:49 +0200

    Exteneded AbstractUser with -> [num_snippets,func->__str__]

--- pastebin_clone/pastebin/models.py
+++ pastebin_clone/pastebin/models.py
@@ -1,1 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class UserModel(AbstractUser):
+    num_snippets = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.username
