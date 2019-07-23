414f91850da3fc923c8f6ac432f94b51d842c594
MohamedAyman3600 <moh.ayman3600@gmail.com>
2019-07-23 20:11:39 +0200

    fresh start

--- pastbinAPI/pastbinAPI/urls.py
+++ pastbinAPI/pastbinAPI/urls.py
@@ -1,4 +1,4 @@
-"""pastebin_clone URL Configuration
+"""pastbinAPI URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
@@ -14,8 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, re_path, include
+from django.urls import path
 
 urlpatterns = [
-    re_path(r'^', include('pastebin.urls')),
+    path('admin/', admin.site.urls),
 ]
--- pastbinAPI/pastbinAPI/wsgi.py
+++ pastbinAPI/pastbinAPI/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for pastebin_clone project.
+WSGI config for pastbinAPI project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pastebin_clone.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pastbinAPI.settings')
 
 application = get_wsgi_application()
