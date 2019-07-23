32f18d1c55d6b4236d98e3f45b5d13099b02737b
MohamedAyman3600 <moh.ayman3600@gmail.com>
2019-07-23 22:30:35 +0200

    added user serializer

--- pastbinAPI/pastebin/serializers.py
+++ pastbinAPI/pastebin/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
 from rest_framework import serializers
 from .models import Snippet
+from django.contrib.auth.models import User
 
 
 class SnipprtSerializer(serializers.ModelSerializer):
     class Meta:
         model = Snippet
         fields = ['id', 'title', 'content', 'created_at']
+
+
+class UserSerializer(serializers.ModelSerializer):
+    model = User
+    fields = ['id', 'username', 'snippets']
