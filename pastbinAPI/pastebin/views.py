9c30d54e79613145040186f351f21910e3d5421d
MohamedAyman3600 <moh.ayman3600@gmail.com>
2019-07-24 00:25:20 +0200

    Remove api_root as url Router creates it

--- pastbinAPI/pastebin/views.py
+++ pastbinAPI/pastebin/views.py
@@ -10,14 +10,6 @@ from rest_framework.response import Response
 from rest_framework.reverse import reverse
 
 
-@api_view(['GET'])
-def api_root(request, format=None):
-    return Response({
-        'users': reverse('user-list', request=request, format=format),
-        'snippets': reverse('snippet-list', request=request, format=format)
-    })
-
-
 class SnippetViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Snippet.objects.all()
