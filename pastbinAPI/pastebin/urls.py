4ad5077c6022f1af0afafc8ab72772f4cdb4cf2c
MohamedAyman3600 <moh.ayman3600@gmail.com>
2019-07-24 00:24:34 +0200

    use url Router

--- pastbinAPI/pastebin/urls.py
+++ pastbinAPI/pastebin/urls.py
@@ -1,36 +1,11 @@
 from django.urls import path, include
-from rest_framework.urlpatterns import format_suffix_patterns
-from .views import SnippetViewSet, UserViewSet, api_root
-from rest_framework import renderers
+from . import views
+from rest_framework.routers import DefaultRouter
 
-snippet_list = SnippetViewSet.as_view({
-    'get': 'list',
-    'post': 'create'
-})
-
-snippet_detail = SnippetViewSet.as_view({
-    'get': 'retrieve',
-    'put': 'update',
-    'patch': 'partial_update',
-    'delete': 'destroy'
-})
-
-user_list = UserViewSet.as_view({
-    'get': 'list'
-})
-
-user_detail = UserViewSet.as_view({
-    'get': 'retrieve'
-})
-
+router = DefaultRouter()
+router.register(r'snippets', views.SnippetViewSet)
+router.register(r'users', views.UserViewSet)
 
 urlpatterns = [
-    path('', api_root, name='root'),
-    path('snippets/', snippet_list, name='snippet-list'),
-    path('snippets/<int:pk>/', snippet_detail, name='snippet-detail'),
-    path('users/', user_list, name='user-list'),
-    path('users/<int:pk>/', user_detail, name='user-detail'),
-    path('api-auth/', include('rest_framework.urls'), name='login'),
+    path('', include(router.urls)),
 ]
-
-urlpatterns = format_suffix_patterns(urlpatterns)
