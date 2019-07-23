466d87f83fd116b1c9a5fe3d944e2e5b2b113457
MohamedAyman3600 <moh.ayman3600@gmail.com>
2019-07-23 16:51:06 +0200

    Added SnippetModel wiith-> [url,tirle,created_by, __str__]

--- pastebin_clone/pastebin/models.py
+++ pastebin_clone/pastebin/models.py
@@ -7,3 +7,13 @@ class UserModel(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+class SnippetModel(models.Model):
+    url = models.URLField()
+    title = models.CharField(max_length=100)
+    content = models.CharField(max_length=1000)
+    created_by = models.ForeignKey(UserModel, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
