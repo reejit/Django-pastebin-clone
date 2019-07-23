b5024bcaaf34702d160cbd459301da010133ab2e
MohamedAyman3600 <moh.ayman3600@gmail.com>
2019-07-23 21:38:31 +0200

    Added SnippetDetail APIView

--- pastbinAPI/pastebin/views.py
+++ pastbinAPI/pastebin/views.py
@@ -1,4 +1,5 @@
 from .models import Snippet
+from django.shortcuts import get_object_or_404
 from .serializers import SnipprtSerializer
 from rest_framework import status
 from rest_framework.views import APIView
@@ -17,3 +18,23 @@ class SnippetList(APIView):
             serializer.sav()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class SnippetDetail(APIView):
+    def get(self, request, pk, format=None):
+        snippet = get_object_or_404(Snippet, pk=pk)
+        serializer = SnipprtSerializer(snippet)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        snippet = get_object_or_404(Snippet, pk=pk)
+        serializer = SnipprtSerializer(snippet, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        snippet = get_object_or_404(Snippet, pk=pk)
+        snippet.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
