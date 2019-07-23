49e2388cde7da67a74d4c2a3dda9fb90d6841e33
MohamedAyman3600 <moh.ayman3600@gmail.com>
2019-07-23 21:54:30 +0200

    update views to use generics

--- pastbinAPI/pastebin/views.py
+++ pastbinAPI/pastebin/views.py
@@ -1,28 +1,13 @@
 from .models import Snippet
 from .serializers import SnipprtSerializer
-from rest_framework import mixins, generics
+from rest_framework import generics
 
 
-class SnippetList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnipprtSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-
-class SnippetDetail(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnipprtSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
