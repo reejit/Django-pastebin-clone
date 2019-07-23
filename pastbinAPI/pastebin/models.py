92b9dd73367589751a60674af6c6dd98c5ea4214
MohamedAyman3600 <moh.ayman3600@gmail.com>
2019-07-23 22:23:29 +0200

    added owner field

--- pastbinAPI/pastebin/models.py
+++ pastbinAPI/pastebin/models.py
@@ -5,6 +5,8 @@ class Snippet(models.Model):
     title = models.CharField(max_length=100, blank=True, default='')
     content = models.TextField()
     created_at = models.DateTimeField(auto_now=True)
+    owner = models.ForeignKey(
+        'auth.User', related_name='snippets', on_delete=models.CASCADE)
 
     def __str__(self):
         if self.title:
