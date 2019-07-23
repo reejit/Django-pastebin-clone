9d2e594ccbdc6a21acd9821525b5c99cd93a57a7
MohamedAyman3600 <moh.ayman3600@gmail.com>
2019-07-23 23:05:24 +0200

    add login url

--- pastbinAPI/pastebin/urls.py
+++ pastbinAPI/pastebin/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
@@ -7,7 +7,8 @@ urlpatterns = [
     path('snippets/', views.SnippetList.as_view(), name='snippet-list'),
     path('snippets/<int:pk>/', views.SnippetDetail.as_view(), name='snippet-detail'),
     path('users/', views.UserList.as_view(), name='user-list'),
-    path('userss/<int:pk>/', views.UserDetail.as_view(), name='user-detail')
+    path('userss/<int:pk>/', views.UserDetail.as_view(), name='user-detail'),
+    path('api-auth/', include('rest_framework.urls'), name='login'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
