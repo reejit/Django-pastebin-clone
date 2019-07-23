bfac7ea7baba0c430207e9588b258af41ece5d08
MohamedAyman3600 <moh.ayman3600@gmail.com>
2019-07-23 17:59:37 +0200

    Added to: SnippetModel->pub_date & UserModel->is_guest

--- pastebin_clone/pastebin/models.py
+++ pastebin_clone/pastebin/models.py
@@ -4,6 +4,8 @@ from django.contrib.auth.models import AbstractUser
 
 class UserModel(AbstractUser):
     num_snippets = models.IntegerField(default=0)
+    # Flag to check if user is guest
+    is_guest = models.BooleanField(default=False)
 
     def __str__(self):
         return self.username
@@ -13,6 +15,7 @@ class SnippetModel(models.Model):
     url = models.URLField()
     title = models.CharField(max_length=100)
     content = models.CharField(max_length=1000)
+    pub_date = models.DateTimeField(auto_now=True)
     created_by = models.ForeignKey(UserModel, on_delete=models.CASCADE)
 
     def __str__(self):
