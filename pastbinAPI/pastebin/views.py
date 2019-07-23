eb553c338dc93f474d49c5a8b9b8c31fbca0f00d
MohamedAyman3600 <moh.ayman3600@gmail.com>
2019-07-24 00:57:22 +0200

    remove unneeded imports

--- pastbinAPI/pastebin/views.py
+++ pastbinAPI/pastebin/views.py
@@ -2,12 +2,8 @@ from .models import Snippet
 from .serializers import SnipprtSerializer, UserSerializer
 from .permissions import IsOwnerOrReadOnly
 from django.contrib.auth.models import User
-from rest_framework import generics
 from rest_framework import viewsets
 from rest_framework import permissions
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework.reverse import reverse
 
 
 class SnippetViewSet(viewsets.ModelViewSet):
