77387570d5105b3825af25d63f21186b18b3e1e4
MohamedAyman3600 <moh.ayman3600@gmail.com>
2019-07-23 23:39:09 +0200

    add api_root view

--- pastbinAPI/pastebin/views.py
+++ pastbinAPI/pastebin/views.py
@@ -4,6 +4,9 @@ from .permissions import IsOwnerOrReadOnly
 from django.contrib.auth.models import User
 from rest_framework import generics
 from rest_framework import permissions
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 
 
 class SnippetList(generics.ListCreateAPIView):
@@ -13,6 +16,14 @@ class SnippetList(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         return serializer.save(owner=self.request.user)
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format)
+    })
 
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
