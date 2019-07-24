a206800b14c8085268402c78d100eba5944b4116
MohamedAyman3600 <moh.ayman3600@gmail.com>
2019-07-24 20:13:20 +0200

    edit snippet viewSet permissions & added get_permissions method

--- pastbinAPI/pastebin/views.py
+++ pastbinAPI/pastebin/views.py
@@ -1,16 +1,21 @@
 from .models import Snippet
 from .serializers import SnipprtSerializer, UserSerializer
-from .permissions import IsOwnerOrReadOnly
+from .permissions import IsOwnerOrReadOnly, IsOwner
 from django.contrib.auth.models import User
 from rest_framework import viewsets
 from rest_framework import permissions
 
 
 class SnippetViewSet(viewsets.ModelViewSet):
-    permission_classes = [
-        permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
     queryset = Snippet.objects.all()
     serializer_class = SnipprtSerializer
+
+    def get_permissions(self):
+        if self.action in ['update', 'partial_update', 'destroy']:
+            permission_classes = [IsOwner]
+        else:
+            permission_classes = [permissions.AllowAny]
+        return [permission() for permission in permission_classes]
 
     def perform_create(self, serializer):
         return serializer.save(owner=self.request.user)
