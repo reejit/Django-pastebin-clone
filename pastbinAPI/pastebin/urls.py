1fcebb01be64d7312e874f3edc5ccb18f67175e9
MohamedAyman3600 <moh.ayman3600@gmail.com>
2019-07-25 15:34:39 +0200

    added urls for login

--- pastbinAPI/pastebin/urls.py
+++ pastbinAPI/pastebin/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from . import views
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
 
 router = DefaultRouter()
 router.register(r'snippets', views.SnippetViewSet)
@@ -8,4 +9,5 @@ router.register(r'users', views.UserViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('login/', obtain_auth_token, name='login')
 ]
