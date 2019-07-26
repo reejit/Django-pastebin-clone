d612c9d55e59087ce8353b98eda2f85c4b72a7c5
MohamedAyman3600 <moh.ayman3600@gmail.com>
2019-07-26 14:32:24 +0200

    added public and share_to fields

--- pastbinAPI/pastebin/models.py
+++ pastbinAPI/pastebin/models.py
@@ -8,6 +8,9 @@ class Snippet(models.Model):
     created_at = models.DateTimeField(default=now)
     owner = models.ForeignKey(
         'auth.User', related_name='snippets', on_delete=models.CASCADE, blank=True, null=True)
+    public = models.BooleanField(default=True)
+    share_to = models.ManyToManyField(
+        'auth.User', related_name='sharedTo', blank=True)
 
     def __str__(self):
         if self.title:
