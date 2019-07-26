b2a0c975735ab183cb0b079250eab4131bde0522
MohamedAyman3600 <moh.ayman3600@gmail.com>
2019-07-26 14:52:24 +0200

    added coreapi docs url

--- pastbinAPI/pastebin/urls.py
+++ pastbinAPI/pastebin/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework.documentation import include_docs_urls
+
 
 from . import views
 
@@ -10,5 +12,6 @@ router.register(r'users', views.UserViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('login/', obtain_auth_token, name='login')
+    path('login/', obtain_auth_token, name='login'),
+    path(r'docs/', include_docs_urls(title='Pastebin API')),
 ]
