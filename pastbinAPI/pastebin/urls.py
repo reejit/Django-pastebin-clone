3fb3549366f586828ce5394d719bc3ae04366f13
MohamedAyman3600 <moh.ayman3600@gmail.com>
2019-07-26 14:33:21 +0200

    reorganise imports

--- pastbinAPI/pastebin/urls.py
+++ pastbinAPI/pastebin/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
-from . import views
 from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken.views import obtain_auth_token
+
+from . import views
 
 router = DefaultRouter()
 router.register(r'snippets', views.SnippetViewSet)
