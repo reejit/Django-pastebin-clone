7ef059727c16821fda5ed2e4a1173dcfd863c25c
MohamedAyman3600 <moh.ayman3600@gmail.com>
2019-07-26 14:35:00 +0200

    reorganise imports & override list and retrieve methods in SnippetViewSet

--- pastbinAPI/pastebin/views.py
+++ pastbinAPI/pastebin/views.py
@@ -1,11 +1,15 @@
+from django.contrib.auth.models import User
+from django.db.models import Q
+from django.shortcuts import get_object_or_404
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import viewsets, mixins
+from rest_framework import permissions
+from rest_framework.response import Response
+
 from .models import Snippet
 from .serializers import SnippetSerializer, UserSerializer
 from .permissions import IsOwnerOrReadOnly, IsOwner
 from .filters import SnippetFilter
-from django.contrib.auth.models import User
-from rest_framework import viewsets, mixins
-from rest_framework import permissions
-from django_filters.rest_framework import DjangoFilterBackend
 
 
 class SnippetViewSet(viewsets.ModelViewSet):
@@ -26,6 +30,24 @@ class SnippetViewSet(viewsets.ModelViewSet):
             return serializer.save()
         else:
             return serializer.save(owner=self.request.user)
+
+    def list(self, request):
+        if self.request.user.is_anonymous:
+            queryset = Snippet.objects.filter(public=True)
+        else:
+            queryset = Snippet.objects.filter(
+                Q(owner=request.user) | Q(public=True) | Q(share_to=request.user))
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        queryset = Snippet.objects.all()
+        snippet = get_object_or_404(queryset, pk=pk)
+        if snippet.owner == request.user or snippet.public or request.user in snippet.share_to.all():
+            serializer = self.get_serializer(snippet)
+            return Response(serializer.data)
+        else:
+            return Response({'data': 'Not permitted'})
 
 
 class UserViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin, mixins.RetrieveModelMixin):
